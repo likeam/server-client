cbad9f939ffe0edee51e6e494fddde8c63fb8cc3
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2025-11-24 14:28:43 +0500

    Add server functions

--- src/app/server-vs-client/page.tsx
+++ src/app/server-vs-client/page.tsx
@@ -1,6 +1,11 @@
 import React from "react";
+import { add, subtract } from "@/utils/index";
 
 const ServerComp = () => {
+  const result1 = add(5, 3);
+  const result2 = subtract(10, 4);
+  console.log(result1);
+  console.log(result2);
   console.log("This is from Server");
   return (
     <div>
