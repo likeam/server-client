fbddec4bd97a6ba034fd9ad5297b504a1d25aba1
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2025-11-27 23:41:46 +0500

    Removing Error

--- src/app/ClientComp/page.tsx
+++ src/app/ClientComp/page.tsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 
 const ClientComp = () => {
   const [Movies, setMovies] = useState("");
-  console.log("This is from client side");
+  console.log("This is from Client side");
   return (
     <div>
       <h1>Hello from Client Component</h1>
